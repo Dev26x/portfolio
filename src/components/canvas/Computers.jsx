b47064f9fca415e4b92dd05cb675cc55370aede8
Dev26x <dev26x@outlook.com>
2023-07-24 15:44:32 +0000

    pc model position edit for mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,7 +20,7 @@ const Computers = ({isMobile}) => {
       <primitive 
         object={computer.scene}
         scale={isMobile ? 0.55 : 0.75}
-        position={isMobile? [0, -2.5, -1.9] : [0, -3.25, -1.5]}
+        position={isMobile? [0, -3.5, -1.9] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
